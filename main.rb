91b3ecad50379bf251a2b505269205e4d05aaec7
Grishchenko Daniil <grishchenko_daniil_1@mail.ru>
2022-06-24 23:42:32 +0300

    version1

---
+++ main.rb
@@ -0,0 +1,6 @@
+#Приветствуем пользователя
+puts File.readlines("#{__dir__}/data/greetings.txt", mode: "r", encoding: "UTF-8").sample
+
+#Даём ответ
+sleep(3)
+puts File.readlines("#{__dir__}/data/answers.txt", mode: "r", encoding: "UTF-8").sample
