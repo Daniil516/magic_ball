a6e5dcdb7ee708f73fedacb4b92eb978e347930b
Grishchenko Daniil <grishchenko_daniil_1@mail.ru>
2022-06-24 23:50:50 +0300

    version1

--- main.rb
+++ main.rb
@@ -4,3 +4,4 @@ puts File.readlines("#{__dir__}/data/greetings.txt", mode: "r", encoding: "UTF-8
 #Даём ответ
 sleep(3)
 puts File.readlines("#{__dir__}/data/answers.txt", mode: "r", encoding: "UTF-8").sample
+sleep(1)
